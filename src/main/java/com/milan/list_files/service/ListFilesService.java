21ebb4528dd0b29d178689aed784300032a0e851
Milan Zaric <milan.zaric@sistemi.rs>
2024-06-27 15:55:04 +0200

    Working code
    Pageable with limit of 10 files per read.

--- src/main/java/com/milan/list_files/service/ListFilesService.java
+++ src/main/java/com/milan/list_files/service/ListFilesService.java
@@ -7,6 +7,10 @@ import com.milan.list_files.repository.ListFilesRepository;
 import jakarta.persistence.EntityExistsException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Value;
@@ -14,7 +18,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 
 import java.io.File;
-import java.util.Arrays;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 @Service
@@ -27,41 +31,58 @@ public class ListFilesService {
     @Value("${folder.path}")
     private String directoryPath;
 
+    private Set<String> processedFiles = new HashSet<>();
+    private int lastProcessedPage = 0;
+
     @Transactional
     @Scheduled(
             timeUnit = TimeUnit.MINUTES,
             initialDelayString = "${list.files.initialDelayMinutes}",
             fixedDelayString = "${list.files.fixedRateMinutes}")
     public void processFiles() {
-        File directory = new File(directoryPath);
-        File[] files = directory.listFiles();
-        System.out.println("Start processing files");
+        log.info("Start processing files");
 
-        if (files != null) {
-            Arrays.stream(files)
-                    .limit(10)
-                    .forEach(file -> {
-                        try {
-                            CallDetailRecordReport record = CallDetailRecordReport.builder()
-                                    .fileName(file.getName())
-                                    .reportStatusType(ReportStatusType.PENDING)
-                                    .build();
-                            listFilesRepository.save(record);
-                            if (listFilesRepository.existsByFileNameAndReportStatusType(file.getName(), ReportStatusType.PENDING)) {
-                                log.warn("File {} already exists with PENDING status, skipping", file.getName());
-                            } else {
-                                log.info("Import file {}", file.getName());
-                            }
+        int pageSize = 10;
 
-                        } catch (EntityExistsException e) {
-                            log.error("File already exists in database: {}", file.getName(), e);
-                        }
+        Pageable pageable = PageRequest.of(lastProcessedPage, pageSize);
 
+        Page<File> page = fetchFilesFromDirectory(directoryPath, pageable);
 
-                    });
-
+        if (!page.isEmpty()) {
+            List<File> filesToProcess = page.getContent();
+            filesToProcess.forEach(file -> {
+                try {
+                    CallDetailRecordReport callDetailRecordReport = CallDetailRecordReport.builder()
+                            .fileName(file.getName())
+                            .reportStatusType(ReportStatusType.PENDING)
+                            .build();
+                    listFilesRepository.save(callDetailRecordReport);
+                    processedFiles.add(file.getName());
+                    log.info("Imported file: {}", file.getName());
+                } catch (EntityExistsException e) {
+                    log.warn("File {} already exists with PENDING status, skipping", file.getName());
+                    log.error("Error message: {}", e.getMessage());
+                } catch (Exception e) {
+                    log.error("Error processing file {}: {}", file.getName(), e.getMessage());
+                }
+            });
+            lastProcessedPage++;
+        } else {
+            log.warn("No files found in directory: {}", directoryPath);
+            lastProcessedPage = 0;
         }
+
+        log.info("Finished processing files");
     }
 
+    private Page<File> fetchFilesFromDirectory(String directoryPath, Pageable pageable) {
+        File directory = new File(directoryPath);
+        File[] files = directory.listFiles();
 
+        List<File> fileList = files != null ? Arrays.asList(files) : Collections.emptyList();
+        int start = (int) pageable.getOffset();
+        int end = Math.min((start + pageable.getPageSize()), fileList.size());
+
+        return new PageImpl<>(fileList.subList(start, end), pageable, fileList.size());
+    }
 }
